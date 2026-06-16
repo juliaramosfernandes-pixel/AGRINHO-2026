ea7a52f796336f9413b7fa11cd1445c699b15eae
juliaramosfernandes-pixel <julia.ramos.fernandes@escola.pr.gov.br>
2026-06-16 11:30:17 -0300

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+// Mensagem de boas-vindas
+window.onload = function () {
+alert("Bem-vindo ao projeto Agrinho 2026!");
+};
 
+// Botão voltar ao topo
+const botaoTopo = document.getElementById("voltarTopo");
+
+if (botaoTopo) {
+botaoTopo.addEventListener("click", function () {
+window.scrollTo({
+top: 0,
+behavior: "smooth"
+});
+});
+}
